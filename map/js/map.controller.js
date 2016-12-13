623f88c4c316bed912e7a6b103fdc7174d57d2d4
Hart <hartlink@gmail.com>
2016-12-13 11:25:39 -0600

    Se agregarón los marcadores correctos.

--- map/js/map.controller.js
+++ map/js/map.controller.js
@@ -1,31 +1,89 @@
 angular.module('map', ['ngMap'])
-.controller('mapController', function ($scope, NgMap) {
+
+.service('mapService',function($http){
+
+    this.markerData = function(){
+	
+	 var url = "http://turiscams.com/tkm/v1/map/marker/";
+	
+			return $http({
+							
+							method : 'GET',
+							url : url
+							}).success(function(response){
+							
+							// Create a object to save retrivied user data and set in cookie.
+                          //  console.log(response['57bb551c8b6c6b9a23dfe813'].Geolocation.latitude);
+                            
+							// console.log( response);
+								return response;
+								
+							}).error(function(response){
+								
+
+
+							}); 
+							
+		
+							}
+	this.dataBinding = function(z){
+
+		var x = z + 3;
+
+		return x;
+
+
+	}
+
+
+
+
+})
+
+
+.controller('mapController', function ($scope, NgMap,mapService,filterService) {
+
+    mapService.markerData().success(function(pinData){
+
+        console.log(pinData[0].Geolocation.latitude);
+ 
 
         NgMap.getMap().then(function (map) {
             $scope.map = map;
             $scope.initMarkerClusterer();
         });
 
+        // $scope.cities = [
+        //     { id: 1, name: 'Oslo', pos: [59.923043, 10.752839] },
+        //     { id: 2, name: 'Stockholm', pos: [59.339025, 18.065818] },
+        //     { id: 3, name: 'Copenhagen', pos: [55.675507, 12.574227] },
+        //     { id: 4, name: 'Berlin', pos: [52.521248, 13.399038] },
+        //     { id: 5, name: 'Paris', pos: [48.856127, 2.346525] }
+        // ];
+
         $scope.cities = [
-            { id: 1, name: 'Oslo', pos: [59.923043, 10.752839] },
-            { id: 2, name: 'Stockholm', pos: [59.339025, 18.065818] },
-            { id: 3, name: 'Copenhagen', pos: [55.675507, 12.574227] },
-            { id: 4, name: 'Berlin', pos: [52.521248, 13.399038] },
-            { id: 5, name: 'Paris', pos: [48.856127, 2.346525] }
-        ];
-        
+
+            {id: pinData[0]._id.$id, name: pinData[0].City, pos: [pinData[0].Geolocation.longitude,pinData[0].Geolocation.latitude]},
+            {id: pinData[1]._id.$id, name: pinData[1].City, pos: [pinData[1].Geolocation.longitude,pinData[1].Geolocation.latitude]},
+            {id: pinData[2]._id.$id, name: pinData[2].City, pos: [pinData[2].Geolocation.longitude,pinData[2].Geolocation.latitude]},
+            
+
+        ];     
+
+
 
 
         $scope.initMarkerClusterer = function () {
             var markers = $scope.cities.map(function (city) {
                 return $scope.createMarkerForCity(city);
             });
-            var mcOptions = { imagePath: 'https://cdn.rawgit.com/googlemaps/js-marker-clusterer/gh-pages/images/m' };
+            var mcOptions = { imagePath: 'map/img/m', };
             return new MarkerClusterer($scope.map, markers, mcOptions);
         };
 
 
         $scope.createMarkerForCity = function (city) {
+            console.log(city.pos[0]);
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(city.pos[0], city.pos[1]),
                 title: city.name
@@ -38,6 +96,9 @@ angular.module('map', ['ngMap'])
         }
 
     });
+
+    }); // End of service of pinData
+        
 
 // angular.module('map')
 // .controller('mapCtrl',function($scope, $state,$http,$timeout){
