7ee5ae18145a8092b2b6a7adfb311f0c3fa4c6ca
Hart <hartlink@gmail.com>
2016-12-27 12:36:29 -0600

    Se agrego spiner cuando se loguea con facebook además se elimino codigo no necesario así como console logs

--- login/js/facebook-login.controller.js
+++ login/js/facebook-login.controller.js
@@ -1,70 +1,15 @@
 angular.module('facebook')
 
-.service('loginService2',function($http, $cookies){
+.controller('facebookCtrl',function($scope,$http,$cookies,$q,$ionicPopup, $timeout, $ionicLoading){
 	
-	this.facebook = function(){
-		
-		console.log("FACEBOOK!");
-		openFB.init({appId: '128116853951759'});
-		openFB.login(
-                function(response) {
-                    if(response.status === 'connected') {
-                       // alert('Facebook login succeeded, got access token: ' + response.authResponse.accessToken);
-                       //location.href = 'home';
-                       console.log(response['authResponse']['accessToken']);
-                       var fb_access_token = response['authResponse']['accessToken'];
-                       
-                       var url = "http://turiscams.com/tkm/v1/users/facebook/";
-				
-						$http({
-							
-							method : 'POST',
-							url : url,
-							headers: { 'Content-Type': 'application/json'},
-							data: { fb_access_token: fb_access_token }
+	// Create function to display the loader if user selects Facebook login
+	$scope.show = function() {
+		$ionicLoading.show({
+			template: ' <md-icon md-svg-src="css/icons/spinner-movil.svg" class="loader-turiscams-icon"></md-icon>'
+		});
+	};	
 
-							}).then(function successCallback(response){
-								
-								
-								// Create a object to save retrivied user data and set in cookie.
-							console.log(response);	
-							var user_data = {
-								
-								user_id: response.data.user_id._id.$id,
-								email: response.data.user_id.email,
-								username: response.data.user_id.user_details.name,
-								profile_pic: response.data.user_id.user_details.profile_url
-								
-							};
-							
-								console.log(user_data);
-							
-								$cookies.putObject('user_data', user_data, {path: "/"});
 
-								location.href = "home"
-								
-							}, function errorCallback(response){
-
-								console.log(response);
-								//alert('El usuario ya existe.');
-								
-							});	
-                       
-                       
-                       
-                       
-                    } else {
-                        alert('Facebook login failed: ' + response.error);
-                    }
-                }, {scope: 'email,read_stream,publish_actions'});
-		
-		
-	}	
-	
-})
-
-.controller('facebookCtrl',function(loginService2, $scope,$http,$cookies){
-	
 	$scope.facebook_login = function(){
 		
 	
@@ -73,11 +18,11 @@ angular.module('facebook')
 		openFB.login(
                 function(response) {
                     if(response.status === 'connected') {
-                       // alert('Facebook login succeeded, got access token: ' + response.authResponse.accessToken);
-                       //location.href = 'home';
-                       console.log(response['authResponse']['accessToken']);
+
+					// Console Log to display facebook response.	
+                    //    console.log(response['authResponse']['accessToken']);
                        var fb_access_token = response['authResponse']['accessToken'];
-                       
+                       $scope.show($ionicLoading);
                        var url = "http://turiscams.com/tkm/v1/users/facebook/";
 				
 						$http({
@@ -90,8 +35,8 @@ angular.module('facebook')
 							}).then(function successCallback(response){
 								
 								
-								// Create a object to save retrivied user data and set in cookie.
-							console.log(response);	
+							// Create a object to save retrivied user data and set in cookie.
+							// console.log(response);	
 							var user_data = {
 								
 								user_id: response.data.user_id._id.$id,
@@ -105,21 +50,23 @@ angular.module('facebook')
 								window.location.href = "home.html";
 
 							}, function errorCallback(response){
-
+								$ionicLoading.hide();
 								console.log(response);
-								//alert('El usuario ya existe.');
+								
 								
 							});	
                        
-                       
+                 
                        
                        
                     } else {
-                        // alert('Facebook login failed: ' + response.error);
+                         console.log('Facebook login failed: ' + response.error);
                     }
                 }, {scope: 'email,read_stream,publish_actions'});
 		
-		
+				
+
+
 	}
 	
 	
